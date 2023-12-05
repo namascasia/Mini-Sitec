2ba4b8652cc71f176a0c0821fb86ce2a4cb5fd32
Jesus Gastelum <gastelumtec21@gmail.com>
2023-12-05 02:17:15 -0700

    change color of menu deppend of the section

--- Cliente/src/components/Menu/Menu.vue
+++ Cliente/src/components/Menu/Menu.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter();
+const route = useRoute();
+
 
 </script>
 
@@ -10,25 +12,25 @@ const router = useRouter();
         <h3>Menu</h3>
         <nav class="nav">
             <ul>
-                <li @click="router.push('/grupos')" class="list_item">
+                <li @click="router.push('/grupos')" class="list_item" :class="route.path === '/grupos' ? 'active' : ''">
                     <img src="/img/user-groups.png" alt="User icon">
                     Grupos
                 </li>
-                <li @click="router.push('/materias')" class="list_item">
+                <li @click="router.push('/materias')" class="list_item" :class="route.path === '/materias' ? 'active' : ''" >
                     <img src="/img/books.png" alt="User icon">
                     Materias
                 </li>
-                <li @click="router.push('/alumnos')" class="list_item">
+                <li @click="router.push('/alumnos')" class="list_item" :class="route.path === '/alumnos' ? 'active' : ''">
                     <img src="/img/user.png" alt="User icon">
                     Alumnos
                 </li>
-                <li @click="router.push('/maestros')" class="list_item">
+                <li @click="router.push('/maestros')" class="list_item" :class="route.path === '/maestros' ? 'active' : ''">
                     <img src="/img/teacher.png" alt="User icon">
                     Maestros
                 </li>
-                <li @click="router.push('/cargaMaterias')" class="list_item">
+                <li @click="router.push('/cargaMaterias')" class="list_item" :class="route.path === '/cargaMaterias' ? 'active' : ''">
                     <img src="/img/subir.png" alt="cargaMateria icon">
-                    Cargar materias
+                    Cargar
                 </li>
             </ul>
         </nav>
